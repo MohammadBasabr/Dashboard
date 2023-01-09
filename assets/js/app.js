7b2b752ff5cd332c165144b6c1ef042cc262d83f
Mohammad Baasabr <m.basabr75@gmail.com>
2023-01-09 11:31:14 +0330

    Layout bugs fixed.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,47 @@
-// chart
-const ctx = document.getElementById('myChart');
+const shrink_btn = document.querySelector(".js-button");
+const sidebar_links = document.querySelector(".cpanel a");
+const active_tab = document.querySelector(".active-tab");
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
+shrink_btn.addEventListener("click", () => {
+  document.body.classList.toggle("shrink");
+  // setTimeout(moveActiveTab, 400);
+
+  shrink_btn.classList.add("hovered");
+
+  setTimeout(() => {
+    shrink_btn.classList.remove("hovered");
+  }, 500);
 });
+
+function showTooltip() {
+  let tooltip = this.parentNode.lastElementChild;
+  let spans = tooltip.children;
+  let tooltipIndex = this.dataset.tooltip;
+
+  Array.from(spans).forEach((sp) => sp.classList.remove("show"));
+  spans[tooltipIndex].classList.add("show");
+
+  tooltip.style.top = `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`;
+}
+
+tooltip_elements.forEach((elem) => {
+  elem.addEventListener("mouseover", showTooltip);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
