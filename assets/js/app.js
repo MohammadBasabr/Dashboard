1410b2229e633ad1845868ce2a29155a06dcb9df
Mohammad Baasabr <m.basabr75@gmail.com>
2023-01-15 14:24:27 +0330

    fix: menu toggler was fastle relocating, fixed.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,11 +7,13 @@ let menu_off = true;
 
 toggle_btn.addEventListener("click", () => {
   if (menu_off) {
+    toggle_btn.classList.toggle("toggle-btn-hide");
     side.classList.toggle("resize-side");
     content.classList.toggle("create-content");
     setTimeout(() => {
       content.classList.toggle("visible-content");
       content.classList.toggle("show-content");
+      toggle_btn.classList.toggle("toggle-btn-hide");
     }, 500);
     menu_off = false;
   } else {
